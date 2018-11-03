3e551afaf10a3bbd8866d7bcc7a3f929047dd443
RinaticState <rinaticstates@gmail.com>
2018-11-03 00:12:42 -0400

    dummy points

--- app.py
+++ app.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world"
+    return "there's nothing here."
+
+@app.route("/hello")
+def hello_word():
+    return "Hello, word"
+
+@app.route("/user/<rina>")
+def show_user_profile(rina):
+    print("Hi there")
+    return 'User %s' % rina
